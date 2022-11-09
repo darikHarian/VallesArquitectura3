147a71df638838e7d6a8115801d6e59eb7829b70
Germán Trigo Cortés <contacto@alphacorp.cl>
2022-11-09 14:03:08 -0300

    Commit V48

--- form-validation.php
+++ form-validation.php
@@ -1,41 +1,29 @@
 <?php
 
-// Main
+// Variables
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-if(isset($_POST['email'])) {
-		
+// Destinatario
 $email_to = "contacto@alphacorp.cl";	
 $email_subject = "Mensaje de cliente desde vallesarquitectura.cl";
 	
-// Validación de datos
-	
-if(!isset($_POST['name']) ||
-   !isset($_POST['subject']) ||
-   !isset($_POST['email']) ||
-   !isset($_POST['message'])) {
-	
-echo "Lo sentimos, hubo un error en sus datos y el formulario no pudo ser enviado en este momento.<br/> <br/>";
-echo "Por favor, corrija sus datos e inténtelo nuevamente.";	
-die();
-}
-	
-// Contenido	
-	
+// Contenido del mensaje	
 $email_message .= "Contenido del Mensaje.\n\n";  
-$email_message .= "Nombre: ".$_POST['name']."\n";
-$email_message .= "Asunto: ".$_POST['subject']."\n";
-$email_message .= "Email: ".$_POST['email']."\n";
-$email_message .= "Mensaje: ".$_POST['message']."\n";
+$email_message .= "Nombre: " . $_POST['name'] ."\n";
+$email_message .= "Asunto: " . $_POST['subject'] ."\n";
+$email_message .= "Email: " . $_POST['email'] ."\n";
+$email_message .= "Mensaje: " . $_POST['message'] ."\n";
  
-// Envío mail
-	
-$headers = 'From: '.$email_from."\r\n". 
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail( $email_to, $email_subject, $email_message, $headers);  
+// Muestra mail en texto plano
+$header .= "Content-Type: text/plain";
 
-echo "<center><b><br/><br/>¡Gracias por preferirnos!...<br/><br/>";
-echo "Muy pronto nos pondremos en contacto con Usted! :)</b></center>";
+// Función mail para envío del mensaje
+mail($email_to, $email_subject, utf8_decode($email_message, $header));
 
-}
+// Redirección al enviar el mensaje con éxito
+header('Location:form-success.html');
+
 ?>
