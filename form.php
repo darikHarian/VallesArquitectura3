078f31ae5b28bea9cd3be3a9bdb6691be8a45a4f
Germán Trigo Cortés <contacto@alphacorp.cl>
2022-11-09 23:55:21 -0300

    Commit V54

--- form.php
+++ form.php
@@ -5,23 +5,25 @@ $name = $_POST['name'];
 $subject = $_POST['subject'];
 $email = $_POST['email'];
 $message = $_POST['message'];
+$email_from = $_POST['email'];
 
 // Destinatario
 $email_to = "contacto@alphacorp.cl";	
 $email_subject = "Mensaje de cliente desde vallesarquitectura.cl";
 	
 // Contenido del mensaje	
-$email_message .= "Contenido del Mensaje.\n\n";  
+$email_message .= "Contenido del Mensaje:\n\n";  
 $email_message .= "Nombre: " . $_POST['name'] ."\n";
 $email_message .= "Asunto: " . $_POST['subject'] ."\n";
 $email_message .= "Email: " . $_POST['email'] ."\n";
 $email_message .= "Mensaje: " . $_POST['message'] ."\n";
  
-// Muestra mail en texto plano
-$header .= "Content-Type: text/plain";
 
 // Función mail para envío del mensaje
-mail($email_to, $email_subject, utf8_decode($email_message, $header));
+$headers = 'From: ' .$email_from . "\r\n" . 
+'Reply-To: '. $email_from . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
 
 // Redirección al enviar el mensaje con éxito
 header('Location:form-success.html');
