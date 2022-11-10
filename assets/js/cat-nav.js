736bb6c7da55c9f1e149e148e491dfea63b175fb
Germán Trigo Cortés <contacto@alphacorp.cl>
2022-11-10 12:56:37 -0300

    Commit V59

--- assets/js/cat-nav.js
+++ assets/js/cat-nav.js
@@ -2,22 +2,22 @@ $(document).ready(function(e) {
     $("#proy-frame").fadeToggle(100).delay(200).fadeIn(500);
     
     $('#cat1').on('click', function(){
-        $("#proy-frame").fadeToggle(0).delay(0).fadeToggle(400);
+        $("#proy-frame").fadeToggle(50).delay(50).fadeToggle(400);
         $('#proy-frame').attr('src', 'proyectos/cat-viviendas.html');
     });
 
     $('#cat2').on('click', function(){
-        $("#proy-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#proy-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#proy-frame').attr('src', 'proyectos/cat-clinicas.html');
     });
 
     $('#cat3').on('click', function(){
-        $("#proy-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#proy-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#proy-frame').attr('src', 'proyectos/cat-comercio.html');
     });
 
     $('#cat4').on('click', function(){
-        $("#proy-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#proy-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#proy-frame').attr('src', 'proyectos/cat-parques.html');
     });
 });
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,42 +2,42 @@ $(document).ready(function(e) {
     $("#main-frame").fadeToggle(100).delay(200).fadeIn(500);
     
     $('#nav1').on('click', function(){
-        $("#main-frame").fadeToggle(0).delay(0).fadeToggle(400);
+        $("#main-frame").fadeToggle(50).delay(50).fadeToggle(400);
         $('#main-frame').attr('src', 'proyectos.html');
     });
 
     $('#nav2').on('click', function(){
-        $("#main-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#main-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#main-frame').attr('src', 'quienes-somos.html');
     });
 
     $('#nav3').on('click', function(){
-        $("#main-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#main-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#main-frame').attr('src', 'contacto.html');
     });
     
     $('#nav4').on('click', function(){
-        $("#main-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#main-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#main-frame').attr('src', 'innovacion.html');
     }); 
 
     $('#nav5').on('click', function(){
-        $("#main-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#main-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#main-frame').attr('src', 'servicios.html');
     }); 
 
     $('#nav6').on('click', function(){
-        $("#main-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#main-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#main-frame').attr('src', 'innovacion.html');
     }); 
 
     $('#nav7').on('click', function(){
-        $("#main-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#main-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#main-frame').attr('src', 'servicios.html');
     });
 
     $('#nav8').on('click', function(){
-        $("#main-frame").fadeToggle(0).delay(0).fadeToggle(400);	
+        $("#main-frame").fadeToggle(50).delay(50).fadeToggle(400);	
         $('#main-frame').attr('src', 'contacto.html');
     });
 });
